4314da5c56cc98b280974acf69e7b3a90b4b2209
github2020juan <95059076+github2020juan@users.noreply.github.com>
2021-12-12 20:53:36 -0300

    Add files via upload

---
+++ data/Aeropuertos_7.js
@@ -0,0 +1,9 @@
+var json_Aeropuertos_7 = {
+"type": "FeatureCollection",
+"name": "Aeropuertos_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Aeropuerto Internacional Pudong (PVG)", "Capacidad": "60.098.073", "IATA": "PVG", "OACI": "ZSPD" }, "geometry": { "type": "Point", "coordinates": [ 109.327935071371755, 16.415728309614199 ] } },
+{ "type": "Feature", "properties": { "name": "Aeropuerto Internacional Hongqiao (SHA)", "Capacidad": "28.235.691", "IATA": "SHA", "OACI": "ZSSS" }, "geometry": { "type": "Point", "coordinates": [ 109.327930611127101, 16.415728537589054 ] } }
+]
+}
---
+++ data/Puertos_8.js
@@ -0,0 +1,8 @@
+var json_Puertos_8 = {
+"type": "FeatureCollection",
+"name": "Puertos_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Puerto Internacional de Shangai", "Capacidad": "43,5 Millones de TEU's (2020)", "Administra": "Grupo SIPG (SHANGAI INTERNATIONAL PORT GROUP)", "Tipo": "Maritimo \/ Fluvial", "Dimensione": "20 km de longitud, 20 m de calado, 125 muelles y 19 terminales", "x carga": "Manipulacion de contenedores, cargas a granel, roll on y roll off, y terminales para cruceros." }, "geometry": { "type": "Point", "coordinates": [ 109.327933625918078, 16.415729853166258 ] } }
+]
+}
